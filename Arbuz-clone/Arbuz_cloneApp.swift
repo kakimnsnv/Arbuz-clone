26f470b1e79fffefc596fc5e1c3e930a62cb4e95
Kakimbek Nyssanov <kakimbekn@gmail.com>
2024-05-15 01:18:40 +0500

    Initial Commit

---
+++ Arbuz-clone/Arbuz_cloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Arbuz_cloneApp.swift
+//  Arbuz-clone
+//
+//  Created by kakim nyssanov on 15.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct Arbuz_cloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
